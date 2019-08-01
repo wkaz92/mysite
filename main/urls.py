74982ae23b93c6c368ba46ce23c31f9f3c8f8c74
wkaz92 <williamkazenas@gmail.com>
2019-08-01 14:55:53 -0500

    add login and logout

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+from .import views
 
 app_name = 'main'
 
 urlpatterns = [
-  path('', views.homepage, name = 'homepage'),
-  path('register/x',views.register, name = 'register')
+    path('', views.homepage, name='homepage'),
+    path('register/', views.register, name='register'),
+    path('logout', views.logout_request, name='logout'),
+    path('login/', views.login_request, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,10 +5,12 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
+
 def homepage(request):
-	return render(request = request,
-	template_name='main/home.html',
-	context = {"tutorials":Tutorial.objects.all})
+    return render(request=request,
+                  template_name='main/home.html',
+                  context={'tutorials': Tutorial.objects.all})
+
 
 def register(request):
     if request.method == "POST":
@@ -24,11 +26,38 @@ def register(request):
             for msg in form.error_messages:
                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
-            return render(request = request,
-                          template_name = "main/register.html",
-                          context={"form":form})
+            return render(request=request,
+                          template_name="main/register.html",
+                          context={"form": form})
 
     form = UserCreationForm
-    return render(request = request,
-                  template_name = "main/register.html",
-                  context={"form":form})
+    return render(request=request,
+                  template_name="main/register.html",
+                  context={"form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, 'successfully logged out')
+    return redirect('main:homepage')
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request,
+                  template_name="main/login.html",
+                  context={"form": form})
